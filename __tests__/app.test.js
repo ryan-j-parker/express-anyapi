17149494dd939d622df51eebb74b6e4a1f0a545e
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-03 16:23:49 -0700

    add test for pokemon list

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,18 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('pokemon routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/pokemon should return a list of pokemon', async () => {
+    const response = await request(app).get('/pokemon');
+    const expected = pokemon.map((mon) => {
+      return { id: mon.id, name: mon.name, type: mon.type };
+    });
+    expect(response.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
